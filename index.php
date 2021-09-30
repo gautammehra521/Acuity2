c14aead3778417ff6b23ae286c809582a48ce3b7
gautammehra521 <91669633+gautammehra521@users.noreply.github.com>
2021-09-30 17:30:52 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,256 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Acuity</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Crimson+Text:ital,wght@0,300;0,400;0,500;0,600;0,700;1,400;1,600;1,700&display=swap" rel="stylesheet">    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <div class="wrapper">
+    	<!-- header -->
+    	<div class="logo">
+    		<img src="img/Screenshot 2021-03-27 100914.png" class="photo">
+    		
+            <i class="fa fa-bars"></i>
+
+    	</div>
+        <hr class="horizontal">
+
+
+    	<div class="navbar">
+    		<ul class="unorderlist">
+    			<li class="list"><a href="#" class="anchor">Home</a></li>
+    			<li class="list"><a href="#" class="anchor">About</a></li>
+    			<li class="list"><a href="#" class="anchor">Services</a></li>
+    			<li class="list"><a href="#" class="anchor">Blog</a></li>
+    			<li class="list"><a href="#" class="anchor">Contact</a></li>
+    		</ul>
+    	</div>
+    	<!-- content -->
+    	<div class="content">
+    		<h1 class="heading">Reputation. Respect. Result.</h1>
+    		<p class="paragraph">Lorem Ipsum is simply dummy text of the printing and typesetting industry. It has been the industry's standard dummy text.</p>
+    		<div class="button">
+    			<a href="#" class="anchor-2">Read More</a>
+    		</div>
+    	</div>
+    	<div class="main-content">
+    		<div class="key">
+    		<div class="paper">
+    			<i class="fa fa-balance-scale"></i>
+    			<p class="paragraph-2">My client list is small for individual attention!</p>
+    			<h2 class="heading-3">Trusted Client</h2>
+    		</div>
+    		<div class="paper">
+    			<i class="fa fa-balance-scale"></i>
+    			<p class="paragraph-2">My client list is small for individual attention!</p>
+    			<h2 class="heading-3">Trusted Client</h2>
+    	</div>
+    		<div class="paper">
+    			<i class="fa fa-balance-scale"></i>
+    		    <p class="paragraph-2">My client list is small for individual attention!</p>
+    		    <h2 class="heading-3">Trusted Client</h2>
+    		</div>
+           </div>
+           <div class="section">
+           	<div class="sec-1">
+           		<img src="img/Screenshot 2021-03-27 100835.png" class="imgg">
+           	</div>
+            <div class="sec-2">
+              <h1 class="acuity-heading">Welcome to Acuity</h1>
+              <hr class="hr-acuity">
+              <p class="ppara">Lorem Ipsum is simply dummy text of the printing and typesetting industry. It has been the industry's standard dummy text</p>
+              <div class="key-22">
+                <div class="aero">
+                <i class="fa fa-500px"></i>
+                <p class="laptop">Lorem Ipsum is simply dummy text of the printing and typesetting industry. It has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type </p>
+              </div>
+              <div class="aero">
+                <i class="fa fa-500px"></i>
+                <p class="laptop">Lorem Ipsum is simply dummy text of the printing and typesetting industry. It has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type </p>
+              </div>
+              <div class="aero">
+                <i class="fa fa-500px"></i>
+                <p class="laptop">Lorem Ipsum is simply dummy text of the printing and typesetting industry. It has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type</p>
+              </div>
+            </div>
+            <div class="buttonn"><a href="#" class="oooo">Read More</a></div>
+      		
+           </div>
+    	</div>
+    	<div class="content-2">
+    		<div class="mask-1">
+    			<h1 class="apple">Why Hire Us?</h1> 
+          <div class="sdfgh"><hr class="vivo"></div>
+    			<p class="para">There are many varieties of passages of lorem ipsum available, but the majority have suffer alteration for some form by injected humour,or randomised words which don't look even slightly believable</p>
+
+    			 <p class= "para-2">It has survived not only fLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type </p>
+                 <div class="eye">
+    			 <div class="button">
+                <a href="#" class="anchor-2">Read More</a>
+            </div>
+        </div>
+    		</div>
+    		<div class="mask-2">
+    			<h1 class="para-3">
+    			Some inspirational quote from one of the clients or main lawyer
+    		    </h1>
+    		</div>
+    	</div>
+    	<div class="chair">
+    		<h1 class="cap">Broad Range of Law Services</h1>
+    		<hr class="limca">
+    		<div class="main">
+    		<div class="dell">
+
+    			<h1 class="sec-29">Family Law</h1>
+    			<p class="para-4">Lorem Ipsum is simply dummy text of printing and typesetting industry. It has been the industry's standard dummy text ever since the 1500s,</p>
+    			<div class="log"><a href="#"class="sip">Read More<i class="fa fa-arrow-right"></i></a></div>
+    		</div>
+            <div class="dell"><h1 class="sec-29">Business Law</h1>
+    			<p class="para-4">Lorem Ipsum is simply dummy text of printing and typesetting industry. It has been the industry's standard dummy text ever since the 1500s,</p>
+    			<div class="log"><a href="#"class="sip">Read More<i class="fa fa-arrow-right"></i></a></div>
+    		</div>
+            <div class="dell"><h1 class="sec-29">Trust and Estate</h1>
+    			<p class="para-4">Lorem Ipsum is simply dummy text of printing and typesetting industry. It has been the industry's standard dummy text ever since the 1500s,</p>
+    			<div class="log"><a href="#"class="sip">Read More<i class="fa fa-arrow-right"></i></a></div>
+    		</div>
+            <div class="dell"><h1 class="sec-29">Civil Litigation</h1>
+    			<p class="para-4">Lorem Ipsum is simply dummy text of printing and typesetting industry. It has been the industry's standard dummy text ever since the 1500s,</p>
+    			<div class="log"><a href="#"class="sip">Read More<i class="fa fa-arrow-right "></i></a></div>
+    		</div>
+            </div>
+    	</div>
+    	<div class="sisster">
+        <div class="fixed-width">
+          <h1 class="heading-5">Call Today For Free Consultation</h1>
+        <h1 class="heading-4">1-800-111-1111</h1>
+        <p class="para-5">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+        <div class="link"><a href="#" class="anchor-3">Contact Us Now</a></div>
+        </div>
+    		
+    		
+    	</div>
+        <div class="brother">
+         
+          <h1 class="ghhj">Latest From Blog</h1>
+          <hr class="yknbv">
+          <p class="lkjhgf">We strictly follow the State Board's sanitary and disinfection guidelines.</p>
+            <div class="wire">
+                <div class="tile">
+                    <img src="img/Screenshot 2021-04-13 141154.png" class="fa-instagram">
+                    <h5 class="olmpic"><span class="good">By :</span> John Deo   Janvary 1 2020</h5>
+                    <h5 class="did">Sessions for Attorney General</h5>
+                    <p class="shampy">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+                    <div class="gautam">
+                     <a href="#" class="com">Read More<i class="fa fa-arrow-right "></i></a>
+                 </div>
+                </div>
+                <div class="tile">
+                 <img src="img/Screenshot 2021-04-13 141154.png" class="fa-instagram">
+                 <h5 class="olmpic"><span class="good">By :</span> John Deo   Janvary 1 2020</h5>
+                    <h5 class="did">Sessions for Attorney General</h5>
+                    <p class="shampy">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+
+                    <div class="gautam">
+                     <a href="#" class="com">Read More<i class="fa fa-arrow-right "></i></a>
+                 </div>
+             </div>
+                <div class="tile"> 
+                    <img src="img/Screenshot 2021-04-13 141154.png" class="fa-instagram">
+                    <h5 class="olmpic"><span class="good">By :</span> John Deo   Janvary 1 2020</h5>
+                    <h5 class="did">Sessions for Attorney General</h5>
+                    <p class="shampy">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+                     
+                    <div class="gautam">
+                     <a href="#" class="com">Read More<i class="fa fa-arrow-right "></i></a>
+                 </div>
+                </div>
+              
+            </div>
+          </div>
+        </div>
+    	<!--   fotter  --> 
+              <div class="foot">
+                <div class="fixed-width">
+                
+                <div class="main-kurkure">
+
+                  <div class="kurkure-1"><h1 class="hole">Contact Info</h1>
+                    <hr class="only">
+                      <div class="content-58">
+                      <div class="lf-1"> 
+                       <li class="floor">
+                        <a href="#" class="hen"><i class="fa fa-phone"></i> <br>
+                      </a> <h5 class="shed"><span>1-800-111-1111</span></h5>
+                   </li>
+               </div>
+                        <div class="lf-2">
+                            <li class="footer">
+                            <a href="#" class="ice"><i class="fa fa-envelope"></i> <br>
+                       </a> <h5 class="shed"><span >email@mydomain.com</span></h5>
+                        
+                   </li>
+               </div>
+                        <div class="lf-3">
+                            <li class="fix">
+                            <a href="#" class="lamp"> <i class='fa fa-bomb'></i> <br>
+                       </a> <h5 class="shed"><span>Kensington high st London, UK</span></h5>
+                       
+                    </li>
+                      </div>
+                        </div>
+                          <hr class="iii">
+                       <div class="main-con">
+                           <div class="gon">
+
+                            <ul class="white">
+                              <li class="real">
+                            <a href="#">
+                                <i class="fa fa-facebook"></i></a>
+                            </li>
+                            <li class="real">
+                            <a href="#">
+                                <i class="fa fa-instagram"></i></a>
+                            </li>
+                            <li class="real">
+                            <a href="#">
+                                <i class="fa fa-twitter"></i></a>
+                            </li>
+                            <li class="real">
+                            <a href="#">
+                                <i class="fa fa-instagram"></i></a>
+                            </li>
+                        </ul>
+                            </div>
+
+                            <div class="clad">
+                              <p class="rr">@ 2019. All Rights Reserved. Designed by Kuroit. </p>
+                            </div>
+
+                       </div>
+                  </div>
+                  
+                  <div class="kurkure-2">
+                    <div class="ggg">
+                      <h1 class="headerrrr">Quick Links</h1>
+                      <hr class="dial">
+                      <ul class="zero">
+                      <li class="bag"><a href="#" class="Quick">Home</a></li>
+                      <li class="bag"><a href="#" class="Quick">About</a></li>
+                      <li class="bag"><a href="#" class="Quick">Services</a></li>
+                      <li class="bag"><a href="#" class="Quick">Blog</a></li>
+                      <li class="bag"><a href="#" class="Quick">Contact</a></li>
+                    </ul>
+                  </div>   
+                  </div>  
+
+              </div>
+          </div>
+          </div>
+</body>
+</html>
